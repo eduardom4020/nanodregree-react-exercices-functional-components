de88d7ec3a61e1334f82239b0882a7b0e5fe26a5
Eduardo Melo <eduardom4020@gmail.com>
2019-01-25 20:25:49 +0000

    DONE: Changed classes components to functional components

--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
-import MovieCard from './MovieCard';
+import React from 'react'
+import MovieCard from './MovieCard'
 
-class MovieCardsList extends Component {
-  render() {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
-
-    const { profiles, users, movies } = this.props;
-    const usersByMovie = {};
-
+const MovieCardsList = (props) => {
+    const { profiles, users, movies } = props
+    const usersByMovie = {}
+    
     profiles.forEach(profile => {
       const movieID = profile.favoriteMovieID;
 
@@ -31,11 +24,7 @@ class MovieCardsList extends Component {
       />
     ));
 
-    /*
-    Return JSX
-    */
-    return <ul>{movieCards}</ul>;
-  }
+    return (<ul>{movieCards}</ul>)
 }
 
 export default MovieCardsList;
